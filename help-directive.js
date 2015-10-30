92c6b6eaf38bd6102af72540cde5f0bdc2af9531
Connor Stroomberg <c.stroomberg@gmail.com>
2015-10-30 16:29:24 +0100

    clean up

--- help-directive.js
+++ help-directive.js
@@ -1,14 +1,39 @@
-angular.module('help-directive', ['mm.foundation'])
+angular.module('help-directive', ['mm.foundation', ])
 
-.directive('inlineHelp', [function(){
+.factory('HelpPopupService', [function() {
+  var lexicon = {};
+
+  function getHelpItem(key){
+    return lexicon[key];
+  }
+  function loadLexicon(newLexicon){
+    for(propertyName in newLexicon) {
+      if(newLexicon.hasOwnProperty(propertyName)) {
+        lexicon[propertyName] = newLexicon[propertyName];
+      }
+    }
+  };
+
+  return {
+    getHelpItem: getHelpItem,
+    loadLexicon: loadLexicon
+  };
+
+}])
+
+.directive('inlineHelp', ['HelpPopupService', function(HelpPopupService) {
   return {
     restrict: 'AE',
-    replace:false,
-    template: '<a popover="{{helpText}}" ><i class="fa  fa-question-circle"></i></a>',
+    replace: false,
+    template: '<a popover="{{helpText}}" popover-append-to-body="true" popover-animation="true" popover-title="{{title}}" popover-link="{{link}}"><i class="fa  fa-question-circle"></i></a>',
     scope: {
       helpKey: '@'
-    }, link: function(scope) {
-      scope.helpText = 'blabla';
+    },
+    link: function(scope) {
+      var helpItem = HelpPopupService.getHelpItem(scope.helpKey);
+      scope.title = helpItem.title;
+      scope.helpText = helpItem.text;
+      scope.link = helpItem.link;
     }
   }
 }])
