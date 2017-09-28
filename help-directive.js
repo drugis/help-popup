d5c7313ae2a5dc43db7d33f3c8c6f4ff9bd90bbd
Joris de Keijser <chinook25@gmail.com>
2017-09-28 16:41:10 +0200

    foundation upgrade wip

--- help-directive.js
+++ help-directive.js
@@ -1,48 +1,64 @@
+'use strict';
+/* globals angular */
 angular.module('help-directive', ['mm.foundation'])
 
-.factory('HelpPopupService', ['$q', function($q) {
-  var lexicons = {};
-  var promises = [];
+  .factory('HelpPopupService', ['$q', function($q) {
+    var lexicons = {};
+    var promises = [];
 
-  function getHelpItem(key) {
-    return $q.all(promises).then(function() {
-      return lexicons[key];
-    });
-  }
-
-  function loadLexicon(newLexicon) {
-    promises.push(newLexicon);
-    newLexicon.then(function(getResult) {
-      var lexicon = getResult.data;
-      for (propertyName in lexicon) {
-        if (lexicon.hasOwnProperty(propertyName)) {
-          lexicons[propertyName] = lexicon[propertyName];
-        }
-      }
-    });
-  };
-
-  return {
-    getHelpItem: getHelpItem,
-    loadLexicon: loadLexicon
-  };
-}])
-
-.directive('inlineHelp', ['HelpPopupService', function(HelpPopupService) {
-  return {
-    restrict: 'AE',
-    replace: false,
-    template: '<a popover="{{helpText}}" popover-append-to-body="true" popover-animation="true" popover-title="{{title}}" popover-link="{{link}}"><i class="fa  fa-question-circle"></i></a>',
-    scope: {
-      helpKey: '@'
-    },
-    link: function(scope) {
-      HelpPopupService.getHelpItem(scope.helpKey).then(function(helpItem) {
-        scope.title = helpItem.title;
-        scope.helpText = helpItem.text;
-        scope.link = helpItem.link;
+    function getHelpItem(key) {
+      return $q.all(promises).then(function() {
+        return lexicons[key];
       });
-
     }
-  }
-}]);
+
+    function loadLexicon(newLexicon) {
+      promises.push(newLexicon);
+      newLexicon.then(function(getResult) {
+        var lexicon = getResult.data;
+        for (var propertyName in lexicon) {
+          if (lexicon.hasOwnProperty(propertyName)) {
+            lexicons[propertyName] = lexicon[propertyName];
+          }
+        }
+      });
+    }
+
+    return {
+      getHelpItem: getHelpItem,
+      loadLexicon: loadLexicon
+    };
+  }])
+
+  .directive('inlineHelp', ['HelpPopupService',
+    function(HelpPopupService) {
+      return {
+        restrict: 'AE',
+        replace: false,
+        // template: '<a popover="{{helpText}}" popover-append-to-body="true" popover-animation="true" popover-title="{{title}}" popover-link="{{link}}"><i class="fa  fa-question-circle"></i></a>',
+        template: '' +
+          '<a class="has-tip"' +
+          ' tooltip-placement="bottom"' +
+          ' tooltip-trigger="click" ' +
+          ' tooltip-append-to-body="true"' +
+          ' tooltip-html-unsafe="{{helpText}}">' +
+          ' <i class="fa fa-question-circle"></i>' +
+          '</a>',
+        scope: {
+          helpKey: '@'
+        },
+        link: function(scope) {
+          HelpPopupService.getHelpItem(scope.helpKey).then(function(helpItem) {
+            scope.title = helpItem.title;
+            scope.link = helpItem.link;
+            scope.helpText = '' +
+              '<h4>' + helpItem.title + '</h4>' +
+              '<div>' + helpItem.text + '</div>' +
+              '<br><div class="manual-link"><h6><a href="' + helpItem.link +
+              '  " target="_blank">View in manuel <i class="fa fa-external-link"></i></a></h6>' +
+              '</div>';
+          });
+        }
+      };
+    }
+  ]);
