dc7f8faa9d242f2c8992f08f7ddf67d01af50d30
Daan Reid <osmosisch@gmail.com>
2018-08-23 10:23:20 +0200

    remove bower_components

--- help-directive.js
+++ help-directive.js
@@ -1,61 +1,63 @@
 'use strict';
 /* globals angular */
-angular.module('help-directive', ['mm.foundation'])
+define(['angular', 'angular-foundation'], function() {
+  angular.module('help-directive', ['mm.foundation'])
 
-  .factory('HelpPopupService', ['$q', function($q) {
-    var lexicons = {};
-    var promises = [];
+    .factory('HelpPopupService', ['$q', function($q) {
+      var lexicons = {};
+      var promises = [];
 
-    function getHelpItem(key) {
-      return $q.all(promises).then(function() {
-        return lexicons[key];
-      });
-    }
+      function getHelpItem(key) {
+        return $q.all(promises).then(function() {
+          return lexicons[key];
+        });
+      }
 
-    function loadLexicon(newLexicon) {
-      promises.push(newLexicon);
-      newLexicon.then(function(getResult) {
-        var lexicon = getResult.data;
-        for (var propertyName in lexicon) {
-          if (lexicon.hasOwnProperty(propertyName)) {
-            lexicons[propertyName] = lexicon[propertyName];
+      function loadLexicon(newLexicon) {
+        promises.push(newLexicon);
+        newLexicon.then(function(getResult) {
+          var lexicon = getResult.data;
+          for (var propertyName in lexicon) {
+            if (lexicon.hasOwnProperty(propertyName)) {
+              lexicons[propertyName] = lexicon[propertyName];
+            }
           }
-        }
-      });
-    }
+        });
+      }
 
-    return {
-      getHelpItem: getHelpItem,
-      loadLexicon: loadLexicon
-    };
-  }])
-
-  .directive('inlineHelp', ['HelpPopupService',
-    function(HelpPopupService) {
       return {
-        restrict: 'AE',
-        replace: false,
-        template: '' +
-          '<a class="has-tip"' +
-          ' tooltip-trigger="click"' +
-          ' tooltip-append-to-body="true"' +
-          ' tooltip-animation="true"'+
-          ' tooltip-html-unsafe="{{helpText}}">' +
-          ' <i class="fa fa-question-circle"></i>' +
-          '</a>',
-        scope: {
-          helpKey: '@'
-        },
-        link: function(scope) {
-          HelpPopupService.getHelpItem(scope.helpKey).then(function(helpItem) {
-            scope.helpText = '' +
-              '<h4>' + helpItem.title + '</h4>' +
-              '<div class="tooltip-content">' + helpItem.text + '</div>' +
-              '<br><div class="manual-link"><h6><a href="' + helpItem.link +
-              '  " target="_blank">View in manual <i class="fa fa-external-link"></i></a></h6>' +
-              '</div>';
-          });
-        }
+        getHelpItem: getHelpItem,
+        loadLexicon: loadLexicon
       };
-    }
-  ]);
+    }])
+
+    .directive('inlineHelp', ['HelpPopupService',
+      function(HelpPopupService) {
+        return {
+          restrict: 'AE',
+          replace: false,
+          template: '' +
+            '<a class="has-tip"' +
+            ' tooltip-trigger="click"' +
+            ' tooltip-append-to-body="true"' +
+            ' tooltip-animation="true"' +
+            ' tooltip-html-unsafe="{{helpText}}">' +
+            ' <i class="fa fa-question-circle"></i>' +
+            '</a>',
+          scope: {
+            helpKey: '@'
+          },
+          link: function(scope) {
+            HelpPopupService.getHelpItem(scope.helpKey).then(function(helpItem) {
+              scope.helpText = '' +
+                '<h4>' + helpItem.title + '</h4>' +
+                '<div class="tooltip-content">' + helpItem.text + '</div>' +
+                '<br><div class="manual-link"><h6><a href="' + helpItem.link +
+                '  " target="_blank">View in manual <i class="fa fa-external-link"></i></a></h6>' +
+                '</div>';
+            });
+          }
+        };
+      }
+    ]);
+});
